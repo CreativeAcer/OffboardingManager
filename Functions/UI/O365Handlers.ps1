0d1922d407b70b15cf83f872c0d557302a6cf735
Marco <marcomoris@hotmail.com>
2024-12-31 23:18:36 +0100

    O365 loading windows
    Updated the O365 loading windows to work in other thread

--- Functions/UI/O365Handlers.ps1
+++ Functions/UI/O365Handlers.ps1
@@ -235,98 +235,186 @@ function Start-O365Tasks {
     )
 
     try {
+        # Capture values before dispatcher invoke (separate thread)
+        $localSelectedUser = $script:SelectedUser
+        $localDemoMode = $script:DemoMode
+        $localO365Connected = $script:O365Connected
+        $localUseADModule = $script:UseADModule
+        $results = @()
+
+        # Show loading screen
+        $loadingWindow = Show-LoadingScreen -Message "Starting O365 tasks..."
+        $loadingWindow.Show()
+
+        # Force UI update
+        [System.Windows.Forms.Application]::DoEvents()
+        Start-Sleep -Milliseconds 100
+
         # Validate user selection
-        if (-not $script:SelectedUser) {
+        if (-not $localSelectedUser) {
             $script:txtO365Results.Text = "Please select a user first."
+            if ($loadingWindow) { $loadingWindow.Close() }
             return
         }
 
-        if (-not ($script:DemoMode -or $script:O365Connected)) {
+        if (-not ($localDemoMode -or $localO365Connected)) {
             $script:txtO365Results.Text = "Please connect to O365 first."
+            if ($loadingWindow) { $loadingWindow.Close() }
             return
         }
 
-        # Get user details
-        $userEmail = if ($script:DemoMode) {
-            $script:SelectedUser.EmailAddress
-        }
-        else {
-            if ($script:UseADModule) {
-                $script:SelectedUser.mail
+        # Get user email
+        $userEmail = if ($localDemoMode) {
+            $localSelectedUser.EmailAddress
+        } else {
+            if ($localUseADModule) {
+                $localSelectedUser.mail
             } else {
-                $script:SelectedUser.Properties["mail"][0]
+                $localSelectedUser.Properties["mail"][0]
             }
         }
 
         if (-not $userEmail) {
             $script:txtO365Results.Text = "Selected user does not have an email address."
+            if ($loadingWindow) { $loadingWindow.Close() }
             return
         }
 
-        $results = @()
+        # Debug logging
+        Write-Host "Starting task checks..."
 
-        # Execute selected tasks
-        if ($script:chkO365Status.IsChecked) {
-            if ($script:DemoMode) {
+        # Check if any tasks are selected
+        # Create a reference type to store the state
+        $stateRef = [ref]@{
+            HasSelectedTasks = $false
+            UIState = $null
+        }
+        $script:mainWindow.Dispatcher.Invoke([System.Action]{
+            # Debug logging for checkbox states
+            Write-Host "O365Status: $($script:chkO365Status.IsChecked)"
+            Write-Host "ConvertShared: $($script:chkConvertShared.IsChecked)"
+            Write-Host "SetForwarding: $($script:chkSetForwarding.IsChecked)"
+            Write-Host "AutoReply: $($script:chkAutoReply.IsChecked)"
+            Write-Host "RemoveTeams: $($script:chkRemoveTeams.IsChecked)"
+            Write-Host "TransferTeams: $($script:chkTransferTeams.IsChecked)"
+            Write-Host "RemoveSharePoint: $($script:chkRemoveSharePoint.IsChecked)"
+            Write-Host "ReassignLicense: $($script:chkReassignLicense.IsChecked)"
+            Write-Host "DisableProducts: $($script:chkDisableProducts.IsChecked)"
+
+            $stateRef.Value.HasSelectedTasks = $false
+            if ($script:chkO365Status.IsChecked -or 
+                $script:chkConvertShared.IsChecked -or 
+                $script:chkSetForwarding.IsChecked -or 
+                $script:chkAutoReply.IsChecked -or 
+                $script:chkRemoveTeams.IsChecked -or 
+                $script:chkTransferTeams.IsChecked -or 
+                $script:chkRemoveSharePoint.IsChecked -or 
+                $script:chkReassignLicense.IsChecked -or 
+                $script:chkDisableProducts.IsChecked) {
+                $stateRef.Value.HasSelectedTasks = $true
+            }
+            Write-Host "Has selected tasks: $($stateRef.Value.HasSelectedTasks)"
+            # Store UI state
+            $stateRef.Value.UIState = @{
+                CheckboxStates = @{
+                    O365Status = $script:chkO365Status.IsChecked
+                    ConvertShared = $script:chkConvertShared.IsChecked
+                    SetForwarding = $script:chkSetForwarding.IsChecked
+                    AutoReply = $script:chkAutoReply.IsChecked
+                    RemoveTeams = $script:chkRemoveTeams.IsChecked
+                    TransferTeams = $script:chkTransferTeams.IsChecked
+                    RemoveSharePoint = $script:chkRemoveSharePoint.IsChecked
+                    ReassignLicense = $script:chkReassignLicense.IsChecked
+                    DisableProducts = $script:chkDisableProducts.IsChecked
+                }
+                ForwardingEmail = $script:cmbForwardingUser.SelectedItem
+                AutoReplyMessage = $script:txtAutoReplyMessage.Text
+                TeamsOwner = $script:cmbTeamsOwner.SelectedItem
+                LicenseTarget = $script:cmbLicenseTarget.SelectedItem
+                SelectedProducts = @($script:lstProducts.SelectedItems)
+            }
+        })
+        Write-Host "After dispatcher, hasSelectedTasks: $($stateRef.Value.HasSelectedTasks)"
+
+        if (-not $stateRef.Value.HasSelectedTasks) {
+            $script:txtO365Results.Text = "Please select at least one task to run for user $userEmail"
+            if ($loadingWindow) { $loadingWindow.Close() }
+            return
+        }
+
+        # Execute O365 Status Check
+        if ($stateRef.Value.UIState.CheckboxStates.O365Status) {
+            Update-LoadingMessage -LoadingWindow $loadingWindow -Message "Checking O365 status..."
+            if ($localDemoMode) {
                 $o365User = Get-MockO365User -UserPrincipalName $userEmail
                 $results += "O365 Status for $($o365User.DisplayName) (Demo):`n"
                 $results += "- User Principal Name: $($o365User.UserPrincipalName)`n"
                 $results += "- Email: $($o365User.Mail)`n"
                 $results += "- Account Enabled: $($o365User.AccountEnabled)`n"
                 $results += "- Licenses: Office 365 E5`n"
-            }
-            else {
+            } else {
                 try {
                     $o365User = Get-MgUser -Filter "mail eq '$userEmail'" -Property displayName, userPrincipalName, accountEnabled, mail
                     $results += "O365 Status for $($o365User.DisplayName):`n"
                     $results += "- User Principal Name: $($o365User.UserPrincipalName)`n"
                     $results += "- Email: $($o365User.Mail)`n"
                     $results += "- Account Enabled: $($o365User.AccountEnabled)`n"
-                }
-                catch {
+                } catch {
                     $results += "Error retrieving O365 status: $($_.Exception.Message)`n"
                 }
             }
-            
         }
 
-        if ($script:chkConvertShared.IsChecked -or $script:chkSetForwarding.IsChecked -or $script:chkAutoReply.IsChecked) {
+        # Execute Mailbox Management Tasks
+        if ($stateRef.Value.UIState.CheckboxStates.ConvertShared -or 
+            $stateRef.Value.UIState.CheckboxStates.SetForwarding -or 
+            $stateRef.Value.UIState.CheckboxStates.AutoReply) {
+            
+            Update-LoadingMessage -LoadingWindow $loadingWindow -Message "Updating O365 Mailbox..."
             $mailboxResult = Set-O365MailboxManagement `
                 -UserPrincipalName $userEmail `
-                -ConvertToShared $script:chkConvertShared.IsChecked `
-                -SetForwarding $script:chkSetForwarding.IsChecked `
-                -ForwardingEmail $script:txtForwardingEmail.Text `
-                -SetAutoReply $script:chkAutoReply.IsChecked `
-                -AutoReplyMessage $script:txtAutoReplyMessage.Text
-        
+                -ConvertToShared $stateRef.Value.UIState.CheckboxStates.ConvertShared `
+                -SetForwarding $stateRef.Value.UIState.CheckboxStates.SetForwarding `
+                -ForwardingEmail $stateRef.Value.UIState.ForwardingEmail `
+                -SetAutoReply $stateRef.Value.UIState.CheckboxStates.AutoReply `
+                -AutoReplyMessage $stateRef.Value.UIState.AutoReplyMessage
+            
             $results += $mailboxResult + "`n"
         }
 
-        if ($script:chkRemoveTeams.IsChecked -or $script:chkTransferTeams.IsChecked -or $script:chkRemoveSharePoint.IsChecked) {
+        # Execute Teams Management Tasks
+        if ($stateRef.Value.UIState.CheckboxStates.RemoveTeams -or 
+            $stateRef.Value.UIState.CheckboxStates.TransferTeams -or 
+            $stateRef.Value.UIState.CheckboxStates.RemoveSharePoint) {
+            
+            Update-LoadingMessage -LoadingWindow $loadingWindow -Message "Updating O365 Teams..."
             $teamsResult = Set-TeamsManagement `
                 -UserPrincipalName $userEmail `
-                -RemoveFromTeams $script:chkRemoveTeams.IsChecked `
-                -TransferOwnership $script:chkTransferTeams.IsChecked `
-                -NewOwner $script:cmbTeamsOwner.SelectedItem `
-                -RemoveSharePoint $script:chkRemoveSharePoint.IsChecked
-        
+                -RemoveFromTeams $stateRef.Value.UIState.CheckboxStates.RemoveTeams `
+                -TransferOwnership $stateRef.Value.UIState.CheckboxStates.TransferTeams `
+                -NewOwner $stateRef.Value.UIState.TeamsOwner `
+                -RemoveSharePoint $stateRef.Value.UIState.CheckboxStates.RemoveSharePoint
+            
             $results += $teamsResult + "`n"
         }
 
-        if ($script:chkReassignLicense.IsChecked -or $script:chkDisableProducts.IsChecked) {
+        # Execute License Management Tasks
+        if ($stateRef.Value.UIState.CheckboxStates.ReassignLicense -or 
+            $stateRef.Value.UIState.CheckboxStates.DisableProducts) {
+            
+            Update-LoadingMessage -LoadingWindow $loadingWindow -Message "Updating O365 Licensing..."
             $licenseResult = Set-LicenseManagement `
                 -UserPrincipalName $userEmail `
-                -ReassignLicenses $script:chkReassignLicense.IsChecked `
-                -TargetUser $script:cmbLicenseTarget.SelectedItem `
-                -DisableProducts $script:chkDisableProducts.IsChecked `
-                -ProductsToDisable ($script:lstProducts.SelectedItems | ForEach-Object { $_ })
+                -ReassignLicenses $stateRef.Value.UIState.CheckboxStates.ReassignLicense `
+                -TargetUser $stateRef.Value.UIState.LicenseTarget `
+                -DisableProducts $stateRef.Value.UIState.CheckboxStates.DisableProducts `
+                -ProductsToDisable $stateRef.Value.UIState.SelectedProducts
 
             $results += $licenseResult + "`n"
         }
 
-        if ($results.Count -eq 0) {
-            $script:txtO365Results.Text = "Please select at least one task to run for user $userEmail"
-        } else {
+        # Update UI with results
+        $script:mainWindow.Dispatcher.Invoke([System.Action]{
             $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
             $resultText = @"
 Execution Time: $timestamp
@@ -336,10 +424,15 @@ Task Results:
 $($results -join "`n")
 "@
             $script:txtO365Results.Text = $resultText
-        }
+        })
     }
     catch {
         $script:txtO365Results.Text = "Error executing O365 tasks: $($_.Exception.Message)"
         Write-ErrorLog -ErrorMessage $_.Exception.Message -Location "O365-Tasks"
+    }
+    finally {
+        if ($loadingWindow) {
+            $loadingWindow.Close()
+        }
     }
 }
